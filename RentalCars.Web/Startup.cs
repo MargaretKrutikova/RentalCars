54fe80bb9585b923a2ce0b87b0766b1f80f167d8
Margarita Krutikova <margaretkru@gmail.com>
2021-05-02 17:43:45 +0200

    Include swagger ui in prod for testing purposes

--- RentalCars.Web/Startup.cs
+++ RentalCars.Web/Startup.cs
@@ -48,10 +48,11 @@ namespace RentalCars.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RentalCars v1"));
             }
-
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RentalCars v1"));
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
